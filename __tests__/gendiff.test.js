7dd25baa02196f7e91cff8c922f5c84d16f2aefb
Oleg <MNKS@list.ru>
2019-10-14 17:46:58 +1000

    fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,8 +1,17 @@
 import fs from 'fs';
 import compare from '../src/compare';
 
+const result = `
+{
+   host: hexlet.io
+ + timeout: 20
+ - timeout: 50
+ - proxy: 123.234.53.22
+ - follow: false
+ + verbose: true
+}`;
 test('compare 2 plain json', () => {
-  const path = '/mnt/c/StudyAndProjects/Projects/github/frontend-project-lvl2/__tests__/__fixtures__/compare.result.json';
-  const result = fs.readFileSync(path).toString();
-  expect(compare('./json/before.json', './json/after.json')).toBe(result);
+  // const path = '__fixtures__/compare.result.json';
+  // const result = fs.readFileSync(path).toString();
+  expect(compare('./json/before.json', './json/after.json')).toBe(result.trim());
 });
