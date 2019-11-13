6faee5a9c55fdae22e4ea6ec4071b9289df79dd5
Oleg <MNKS@list.ru>
2019-11-13 19:29:25 +1000

    Revert "refactor tests"
    This reverts commit b446da0fe6058352de7a15d620e8929fdf10766b.

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,47 +2,54 @@ import fs from 'fs';
 import path from 'path';
 import compareFiles from '../src/extensionManager';
 
-const getFilePath = (fileName) => path.join(__dirname, `../__fixtures__/${fileName}`);
+const pathToLinearResult = path.resolve('__tests__/__fixtures__/linear-result.txt');
+const pathToPlainResult = path.resolve('__tests__/__fixtures__/plain-result.txt');
+const pathToTreeResult = path.resolve('__tests__/__fixtures__/tree-result.txt');
+const pathToJsonResult = path.resolve('__tests__/__fixtures__/json-result.json');
 
-const getDiff = (fileType, format, output = 'default') => {
-  const beforeValuePath = getFilePath(`${fileType}/before.${format}`);
-  const afterValuePath = getFilePath(`${fileType}/after.${format}`);
+const linearResult = fs.readFileSync(pathToLinearResult).toString().replace(/\s/g, '');
+const plainResult = fs.readFileSync(pathToPlainResult).toString().replace(/\s/g, '');
+const treeResult = fs.readFileSync(pathToTreeResult).toString().replace(/\s/g, '');
+const jsonResult = fs.readFileSync(pathToJsonResult).toString().replace(/\s/g, '');
 
-  const diffFile = compareFiles(beforeValuePath, afterValuePath, output).replace(/\s/g, '');
+const plainBefore = path.resolve('__tests__/__fixtures__/linear/before');
+const plainAfter = path.resolve('__tests__/__fixtures__/linear/after');
 
-  return diffFile;
-};
+const treeBefore = path.resolve('__tests__/__fixtures__/tree/before');
+const treeAfter = path.resolve('__tests__/__fixtures__/tree/after');
 
-const getResult = (fileType) => {
-  const resultValuePath = getFilePath(`${fileType}-result.txt`);
-  const result = fs.readFileSync(resultValuePath, 'utf-8').toString().replace(/\s/g, '');
+test('compare 2 json with plain, tree, linear and json output', () => {
+  const expectedLinearValue = compareFiles(`${plainBefore}.json`, `${plainAfter}.json`).replace(/\s/g, '');
+  const expectedTreeValue = compareFiles(`${treeBefore}.json`, `${treeAfter}.json`).replace(/\s/g, '');
+  const expectedPlainValue = compareFiles(`${treeBefore}.json`, `${treeAfter}.json`, 'plain').replace(/\s/g, '');
+  const expectedJsonValue = compareFiles(`${treeBefore}.json`, `${treeAfter}.json`, 'json').replace(/\s/g, '');
 
-  return result;
-};
-
-test.each([
-  ['linear', 'json', 'default', getResult('linear')],
-  ['tree', 'json', 'default', getResult('tree')],
-  ['tree', 'json', 'plain', getResult('plain')],
-  ['tree', 'json', 'json', getResult('json')],
-])('json (%s, %s, %s)', (type, format, output, expected) => {
-  expect(getDiff(type, format, output)).toBe(expected);
+  expect(expectedLinearValue).toBe(linearResult);
+  expect(expectedTreeValue).toBe(treeResult);
+  expect(expectedPlainValue).toBe(plainResult);
+  expect(expectedJsonValue).toBe(jsonResult);
 });
 
-test.each([
-  ['linear', 'yaml', 'default', getResult('linear')],
-  ['tree', 'yaml', 'default', getResult('tree')],
-  ['tree', 'yaml', 'plain', getResult('plain')],
-  ['tree', 'yaml', 'json', getResult('json')],
-])('yaml (%s, %s, %s)', (type, format, output, expected) => {
-  expect(getDiff(type, format, output)).toBe(expected);
+test('compare 2 yaml with plain, tree and linear output', () => {
+  const expectedLinearValue = compareFiles(`${plainBefore}.yaml`, `${plainAfter}.yaml`).replace(/\s/g, '');
+  const expectedTreeValue = compareFiles(`${treeBefore}.yaml`, `${treeAfter}.yaml`).replace(/\s/g, '');
+  const expectedPlainValue = compareFiles(`${treeBefore}.yaml`, `${treeAfter}.yaml`, 'plain').replace(/\s/g, '');
+  const expectedJsonValue = compareFiles(`${treeBefore}.yaml`, `${treeAfter}.yaml`, 'json').replace(/\s/g, '');
+
+  expect(expectedLinearValue).toBe(linearResult);
+  expect(expectedTreeValue).toBe(treeResult);
+  expect(expectedPlainValue).toBe(plainResult);
+  expect(expectedJsonValue).toBe(jsonResult);
 });
 
-test.each([
-  ['linear', 'ini', 'default', getResult('linear')],
-  ['tree', 'ini', 'default', getResult('tree')],
-  ['tree', 'ini', 'plain', getResult('plain')],
-  ['tree', 'ini', 'json', getResult('json')],
-])('ini (%s, %s, %s)', (type, format, output, expected) => {
-  expect(getDiff(type, format, output)).toBe(expected);
+test('compare 2 ini with plain, tree and linear output', () => {
+  const expectedLinearValue = compareFiles(`${plainBefore}.ini`, `${plainAfter}.ini`).replace(/\s/g, '');
+  const expectedTreeValue = compareFiles(`${treeBefore}.ini`, `${treeAfter}.ini`).replace(/\s/g, '');
+  const expectedPlainValue = compareFiles(`${treeBefore}.ini`, `${treeAfter}.ini`, 'plain').replace(/\s/g, '');
+  const expectedJsonValue = compareFiles(`${treeBefore}.ini`, `${treeAfter}.ini`, 'json').replace(/\s/g, '');
+
+  expect(expectedLinearValue).toBe(linearResult);
+  expect(expectedTreeValue).toBe(treeResult);
+  expect(expectedPlainValue).toBe(plainResult);
+  expect(expectedJsonValue).toBe(jsonResult);
 });
